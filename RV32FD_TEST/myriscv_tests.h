911a97502629f846a770e701976ed24183bf3cd5
Hidetaro Tanaka <s187737r@st.me.tuat.ac.jp>
2021-08-16 17:59:02 +0900

    D拡張テスト作成開始

--- RV32FD_TEST/myriscv_tests.h
+++ RV32FD_TEST/myriscv_tests.h
@@ -1,3 +1,5 @@
+// #define SUPPORT_RV64
+
 // a0 = !(inst(rs1, rs2) == rd);
 // inst: (Float, Float) -> Float
 // 正しければa0=0, 間違っていればa0=1
@@ -80,6 +82,37 @@
     fmv.x.w t4, ft3; \
     xor a0, t3, t4; \
     snez a0, a0;
+
+#ifdef SUPPORT_RV64
+// TODO: RV64用のマクロを追加
+#else
+// a0 = !(inst(rs1, rs2) == rd);
+// inst: (Double, Double) -> Double
+#define TEST_FP_OP2_D(inst, rd_up, rd_down, rs1_up, rs1_down, rs2_up, rs2_down) \
+    li t0, rs1_up; \
+    li t1, rs1_down; \
+    li t2, rs2_up; \
+    li t3, rs2_down; \
+    li a0, rd_up; \
+    li a1, rd_down; \
+    addi t6, sp, -16; \
+    sw t3, 0(t6); \
+    sw t2, 4(t6); \
+    sw t1, 8(t6); \
+    sw t0, 12(t6); \
+    fld ft0, 8(t6); \
+    fld ft1, 0(t6); \
+    inst ft2, ft0, ft1; \
+    fsd ft2, 0(t6); \
+    lw t4, 4(t6); \
+    lw t5, 0(t6); \
+    xor a0, a0, t4; \
+    snez a0, a0; \
+    xor a1, a1, t5; \
+    snez a1, a1; \
+    or a0, a0, a1;
+
+#endif
 
 // print(res * val)
 #define PRINT_IF_ERR( val, res ) \
