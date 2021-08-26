7efa771bafcd304e3b4a0ed50309193ed8e5c689
Hidetaro Tanaka <s187737r@st.me.tuat.ac.jp>
2021-08-26 12:49:55 +0900

    SEMP用ビルドスクリプトのCからアセンブリへの部分にCOMPLER_DIRを適用

--- build_for_SMT.sh
+++ build_for_SMT.sh
@@ -3,7 +3,7 @@
 COMPILER_DIR="/home/hidetaro/docker_volume/semp/opt/riscv32/bin"
 
 $COMPILER_DIR/riscv32-unknown-elf-gcc -c -o smt_test.o $1
-#/home/hidetaro/docker_volume/semp/opt/riscv32/bin/riscv32-unknown-elf-gcc -S -o smt_test.S $1
+# $COMPILER_DIR/riscv32-unknown-elf-gcc -S -o smt_test.S $1
 $COMPILER_DIR/riscv32-unknown-elf-ld -m elf32lriscv smt_test.o _start_SMT.o -L/home/hidetaro/docker_volume/semp/opt/riscv/riscv32-unknown-elf/lib -T link.ld -nostartfiles -static -o smt_test.elf
 $COMPILER_DIR/riscv32-unknown-elf-objcopy -O binary smt_test.elf smt_test.bin
 hexdump -v -e '1/4 "%08x" "\n"' smt_test.bin > smt_test.hex
